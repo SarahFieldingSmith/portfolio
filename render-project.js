d7157deee73586e5574d19ce593d2a4647ea1b3c
Sarah Fielding Smith <sarah@lifeboxhealth.com>
2024-03-02 20:51:15 +0000

    Transfer portfolio

---
+++ render-project.js
@@ -0,0 +1,38 @@
+// note: add data-project attribute to any new projects on project.html
+const projectList = document.querySelectorAll("[data-project]");
+const leftSliderLink = document.querySelector(".left-slider");
+const rightSliderLink = document.querySelector(".right-slider");
+const leftSliderText = document.querySelector(".left-slider-title");
+const rightSliderText = document.querySelector(".right-slider-title");
+
+const urlParams = new URLSearchParams(window.location.search);
+const projectNumber = parseInt(urlParams.get("project"));
+
+// note: add names of any new projects to array
+const projectNames = ["Shortly", "Space", "Portfolio"];
+
+// Render project details depending on URL parameters
+renderProject(projectNumber);
+
+function renderProject(projectNumber) {
+  projectList[projectNumber - 1].removeAttribute("hidden");
+  renderSlider(projectNumber);
+}
+
+function renderSlider(projectNumber) {
+  leftSliderText.innerText = projectNames[projectNumber - 2];
+  rightSliderText.innerText = projectNames[projectNumber];
+
+  leftSliderLink.href = `./project.html?project=${projectNumber - 1}`;
+  rightSliderLink.href = `./project.html?project=${projectNumber + 1}`;
+
+  //   render slider details for first and last project
+  const numberProjects = projectNames.length;
+  if (projectNumber === 1) {
+    leftSliderText.innerText = projectNames[numberProjects - 1];
+    leftSliderLink.href = `./project.html?project=${numberProjects}`;
+  } else if (projectNumber === numberProjects) {
+    rightSliderText.innerText = projectNames[0];
+    rightSliderLink.href = `./project.html?project=1`;
+  }
+}
